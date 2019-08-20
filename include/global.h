335a87e7dc8446eb8a7cafaa4bf554b53e95ef46
Aidan Clyens <aidanc@rogers.com>
2019-08-20 12:50:21 -0400

    Added resources folder

--- include/global.h
+++ include/global.h
@@ -1,12 +1,18 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
+#include <string>
+
 #include "TextureManager.h"
 #include "InputManager.h"
 
+// Definitions
 #define WINDOW_WIDTH    800
 #define WINDOW_HEIGHT   600
 
+const std::string PLAYER_IMG = "res/player.bmp";
+
+// Global variables
 extern InputManager *input_manager;
 extern TextureManager *texture_manager;
 
