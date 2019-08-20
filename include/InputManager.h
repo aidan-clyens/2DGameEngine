24041141b1e23f0f1b777adbd17ed14d42be1d34
Aidan Clyens <aidanc@rogers.com>
2019-08-20 13:49:54 -0400

    Added methods to InputManager

--- include/InputManager.h
+++ include/InputManager.h
@@ -8,11 +8,14 @@ class InputManager {
         InputManager();
         ~InputManager();
 
+        bool get_key_pressed() const;
         sf::Keyboard::Key get_last_key_pressed() const;
 
+        void set_key_pressed(bool is_key_pressed);
         void set_last_key_pressed(sf::Keyboard::Key);
 
     private:
+        bool m_is_key_pressed;
         sf::Keyboard::Key m_last_key_pressed;
 };
 
--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -1,15 +1,25 @@
 #include "InputManager.h"
 
-InputManager::InputManager() {
-
+InputManager::InputManager():
+m_is_key_pressed(false)
+{
+    // Empty
 }
 
 InputManager::~InputManager() {
 
 }
 
+bool InputManager::get_key_pressed() const {
+    return m_is_key_pressed;
+}
+
 sf::Keyboard::Key InputManager::get_last_key_pressed() const {
     return m_last_key_pressed;
+}
+
+void InputManager::set_key_pressed(bool is_key_pressed) {
+    m_is_key_pressed = is_key_pressed;
 }
 
 void InputManager::set_last_key_pressed(sf::Keyboard::Key last_key_pressed) {
